6a3fec49678dbae2c68d4dced8b83ed18b48e25a
Yegoroot <kirsanov.yegor@gmail.com>
2019-05-17 16:36:45 +0300

    home

--- src/assets/jss/material-kit-react/views/components.jsx
+++ src/assets/jss/material-kit-react/views/components.jsx
@@ -1,42 +1,58 @@
 import { container } from 'assets/jss/material-kit-react.jsx'
 
 const componentsStyle = {
-    container,
-    brand: {
-        color: '#FFFFFF',
-        textAlign: 'left',
+  container: {
+    ...container,
+    '&::before': {
+      content: "''",
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      background: '#00000042',
+      backgroundImage: 'linear-gradient(#ffffff1f, #00000061)'
+    }
+  },
+  brand: {
+    color: '#FFFFFF'
+    //   textAlign: "left"
+  },
+  title: {
+    fontSize: '8rem',
+    fontWeight: '600',
+    display: 'inline-block',
+    position: 'relative',
+    '@media (max-width: 760px)': {
+      fontSize: '6rem'
+    }
+  },
+  subTitle: {
+    position: 'relative',
+    fontWeight: 400,
+    marginLeft: 'auto',
+    maxWidth: 740,
+    lineHeight: 1.1
+  },
+  main: {
+    background: '#FFFFFF',
+    position: 'relative',
+    zIndex: '3'
+  },
+  mainRaised: {
+    margin: '-60px 30px 0px',
+    '@media (max-width: 520px)': {
+      margin: '-60px 10px 0px'
     },
-    title: {
-        fontSize: '4.2rem',
-        fontWeight: '600',
-        display: 'inline-block',
-        position: 'relative',
-    },
-    subtitle: {
-        fontSize: '1.313rem',
-        maxWidth: '500px',
-        margin: '10px 0 0',
-    },
-    main: {
-        background: '#FFFFFF',
-        position: 'relative',
-        zIndex: '3',
-    },
-    mainRaised: {
-        margin: '-60px 30px 0px',
-        '@media (max-width: 520px)': {
-            margin: '-60px 10px 0px',
-        },
-        borderRadius: '6px',
-        boxShadow:
-            '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)',
-    },
-    link: {
-        textDecoration: 'none',
-    },
-    textCenter: {
-        textAlign: 'center',
-    },
+    borderRadius: '6px',
+    boxShadow: '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
+  },
+  link: {
+    textDecoration: 'none'
+  },
+  textCenter: {
+    textAlign: 'center'
+  }
 }
 
 export default componentsStyle
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 // nodejs library that concatenates classes
-import classNames from 'classnames'
-// react components for routing our app without refresh
-// import { Link } from 'react-router-dom'
-// @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles'
-// @material-ui/icons
-// core components
 import Header from 'components/Header/Header.jsx'
-import GridContainer from 'components/Grid/GridContainer.jsx'
-import GridItem from 'components/Grid/GridItem.jsx'
 import Parallax from 'components/Parallax/Parallax.jsx'
-// sections for this page
 import HeaderLinks from 'components/Header/HeaderLinks.jsx'
 
 // import { Link } from 'react-router-dom'
 
-// import Footer from 'components/Footer/Footer.jsx'
+// import Footer from "components/Footer/Footer.jsx";
 // import Card from './Sections/Card.jsx'
 
 import componentsStyle from 'assets/jss/material-kit-react/views/components.jsx'
@@ -27,7 +18,7 @@ class Components extends React.Component {
     return (
       <div>
         <Header
-          brand="في منامي"
+          // brand="في منامي"
           isBrandAr
           rightLinks={<HeaderLinks />}
           fixed
@@ -39,21 +30,18 @@ class Components extends React.Component {
           {...rest}
         />
 
-        <Parallax image={require('assets/img/mosque/minaret.jpg')}>
+        <Parallax style={{ height: '100vh' }} image={require('assets/img/bg4.jpg')}>
           <div className={classes.container}>
-            <GridContainer>
-              <GridItem>
-                <div className={classes.brand}>
-                  <h1 className={classes.title}>Заметки Арабского</h1>
-                  <h3 className={classes.subtitle}>Заметки, запсиси, уроки, правила и исключения.</h3>
-                </div>
-              </GridItem>
-            </GridContainer>
+            <div className={classes.brand}>
+              {/* <h1 className={classes.title}>Заметки Арабского</h1> */}
+              <h1 className={classes.title}>في منامي</h1>
+              <h2 className={classes.subTitle}>Заметки, запсиси, уроки, правила и исключения.</h2>
+            </div>
           </div>
         </Parallax>
 
-        <div className="temp">
-          {/* <h4>Третий Том Мединского Курса</h4>
+        {/* <div className="temp">
+          <h4>Третий Том Мединского Курса</h4>
                     Нахв это определение ролей у слов в предложении в то время как сарф это изучение самого слова (морфология)
                     Другими словами Нахв разбирает связи между словами, какмие между слвоами отношения, но сарф это разбор слов индивидуально
                     как будто других слов нет т.е вне зависимости от других слов
@@ -62,19 +50,20 @@ class Components extends React.Component {
                             <Link to="/asd">Собирательная частица [4 урок, нахв]</Link>
                         </li>
                         <li />
-                    </ul> */}
-        </div>
+                    </ul>
+        </div> */}
 
-        <div
+        {/* <div
           className={classNames(classes.main, classes.mainRaised)}
-          style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-          {/* <Card />
+          style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+        >
           <Card />
           <Card />
           <Card />
           <Card />
-          <Card /> */}
-        </div>
+          <Card />
+          <Card />
+        </div> */}
         {/* <Footer /> */}
       </div>
     )
