0d81fa4073b750d613dc56360020a34ed3c884c9
Yegoroot <kirsanov.yegor@gmail.com>
2019-05-19 20:46:01 +0300

    5 names

--- src/views/arBefore7less3Tom/Ism5Page/Ism5Page.jsx
+++ src/views/arBefore7less3Tom/Ism5Page/Ism5Page.jsx
@@ -1,63 +1,81 @@
-import React from "react";
+import React from 'react'
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames'
 
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from '@material-ui/core/styles/withStyles'
 
 // core components
-import Header from "components/Header/Header.jsx";
-import Footer from "components/Footer/Footer.jsx";
+import Header from 'components/Header/Header.jsx'
+import Footer from 'components/Footer/Footer.jsx'
 
-// import Word from 'components/word'
+import Word from 'components/word'
 
-import ArSent from "components/Typography/ArSent.jsx";
+import HeaderLinks from 'components/Header/HeaderLinks.jsx'
 
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import Parallax from 'components/Parallax/Parallax.jsx'
 
-import Parallax from "components/Parallax/Parallax.jsx";
-
-import arabicPageStyle from "assets/jss/material-kit-react/views/arabicPage.jsx";
+import arabicPageStyle from 'assets/jss/material-kit-react/views/arabicPage.jsx'
 
 class Ism5Page extends React.Component {
   render() {
-    const { classes, ...rest } = this.props;
+    const { classes, ...rest } = this.props
 
     return (
       <div>
         <Header
           color="transparent"
           brand="5 имен"
-          isBrandAr
           rightLinks={<HeaderLinks />}
           fixed
           changeColorOnScroll={{
             height: 200,
 
-            color: "white"
+            color: 'white'
           }}
           {...rest}
         />
 
-        <Parallax small filter image={require("assets/img/mosque/mosque17.jpg")} />
+        <Parallax small filter image={require('assets/img/mosque/mosque17.jpg')} />
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div className={classes.container}>
             <div className={classes.navWrapper}>
-              <h1 className={classes.title}>5 имен</h1>
-              <p>В арабском языке есть 5 имен которые по особому склоняются, أب, أخ и другие</p>
+              <h1 className="title">5 имен</h1>
+              <div className="desc">
+                В арабском языке есть 5 имен которые по особому склоняются, <span className="desc__ar"> أب, أخ </span> и другие
+              </div>
 
-              <ArSent translate="المَرْفُوع">جاء أبُوكَ</ArSent>
-
-              <ArSent translate="المَنْصوب">رَأيْتُ أبَاكَ</ArSent>
-
-              <ArSent translate="الجَرُور">قُلْتُ لِأبِيكَ</ArSent>
+              <div className="arsent">
+                <div className="arsent__original">
+                  جاء <Word root="و" suffix="كَ" prefix="أبُ" negative />
+                </div>
+                <div className="arsent__translate">
+                  <span className="desc__ar">(المَرْفُوع)</span>
+                </div>
+              </div>
+              <div className="arsent">
+                <div className="arsent__original">
+                  رَأيْتُ <Word root="ا" suffix="كَ" prefix="أبَ" negative />
+                </div>
+                <div className="arsent__translate">
+                  <span className="desc__ar">(المَنْصوب)</span>
+                </div>
+              </div>
+              <div className="arsent">
+                <div className="arsent__original">
+                  قُلْتُ <Word root="ي" suffix="كَ" prefix="لِأَبِ" negative />
+                </div>
+                <div className="arsent__translate">
+                  <span className="desc__ar">(الجَرُور)</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default withStyles(arabicPageStyle)(Ism5Page);
+export default withStyles(arabicPageStyle)(Ism5Page)
