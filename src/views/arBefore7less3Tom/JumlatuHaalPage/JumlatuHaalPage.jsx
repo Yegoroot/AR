0d8b982502d38a1496a7b44f50a3678f55c1046b
Yegoroot <kirsanov.yegor@gmail.com>
2019-06-08 12:11:00 +0300

    حال

--- src/views/arBefore7less3Tom/JumlatuHaalPage/JumlatuHaalPage.jsx
+++ src/views/arBefore7less3Tom/JumlatuHaalPage/JumlatuHaalPage.jsx
@@ -11,7 +11,8 @@ import Footer from 'components/Footer/Footer.jsx'
 import HeaderLinks from 'components/Header/HeaderLinks.jsx'
 
 import Parallax from 'components/Parallax/Parallax.jsx'
-import AnimateCard from 'components/AnimateCard/AnimateCard.jsx'
+import Word from 'components/word'
+// import AnimateCard from 'components/AnimateCard/AnimateCard.jsx'
 
 import arabicPageStyle from 'assets/jss/material-kit-react/views/arabicPage.jsx'
 
@@ -49,13 +50,12 @@ class JumlatuHaalPage extends React.Component {
                 <b>
                   &nbsp; проясняет состояние владельца (<span className="desc__ar">صاحِبُ الحال</span>)
                 </b>
-                . Отвечает на вопрос
-                <b> "как?"</b>. Само слово
-                <span className="desc__ar"> الحال </span> буквально означает "ситуация" или "положение. (В каком положении пребывает объект,
-                в каком состоянии)
-                {/* <br />
-                Перед
-                <span className="desc__ar"> جملة الحال </span> приходит глагольное предложение в то время как само оно именное */}
+              </div>
+              <div className="desc">
+                Само слово
+                <span className="desc__ar"> الحال </span> буквально означает "ситуация" или "положение. Отвечает на вопрос
+                <b> "как?"</b> (В каком положении пребывает объект, в каком состоянии). Согласуется в роде, числе с (
+                <span className="desc__ar">صاحِبُ الحال</span>)
               </div>
 
               <h2 className="h2ar title">صاحِبُ الحال</h2>
@@ -132,38 +132,181 @@ class JumlatuHaalPage extends React.Component {
                 </div>
               </div>
 
-              <h2 className=" title"> Состояние صاحِبِ الحالِ </h2>
+              <h2 className=" title">
+                Состояние <span className="h2ar"> صاحِبِ الحالِ </span>
+              </h2>
               <div className="desc">
-                В основе <span className="desc__ar"> صاحِبِ الحالِ </span> владелец <span className="desc__ar"> الحالِ </span> приходит в
-                определённом состоянии, кроме случев когда есть какой то разрешитель (<span className="desc__ar"> مُسَوِّغ </span>).
+                В основе <span className="desc__ar"> صاحِبِ الحالِ </span> приходит в определённом состоянии, кроме случев когда есть какой
+                то разрешитель (<span className="desc__ar"> مُسَوِّغ </span>):
+              </div>
+
+              <h3 className="title ">
+                Если <span className="h3ar">الحالِ</span> прыгнет вперед
+              </h3>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> جَاءَني </span>
+                  <span className="contrast"> سائلاً </span>
+                  <span className="contrast_2"> طالبٌ </span>
+                </div>
+                <div className="arsent__translate">
+                  Пришёл ко мне <span className="contrast"> спрашивающий </span> студент
+                </div>
+              </div>
+
+              <h3 className="title ">
+                Если <span className="h3ar">صاحِبِ الحالِ</span> в идафе или с определением
+              </h3>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> جَاءَني </span>
+                  <span className="contrast_2"> طالبٌ مُواظِبٌ </span>
+                  <span className="contrast"> مُسْتَأذِناً </span>
+                  <span> في الخُرُوج </span>
+                </div>
+                <div className="arsent__translate">
+                  Пришёл ко мне <span className="contrast"> просящийся </span> войти, старательный студент
+                </div>
+              </div>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> جَاءَني </span>
+                  <span className="contrast_2"> طَبيبُ الأسْنَانٍ </span>
+                  <span className="contrast"> مُسْتَفْتِياً </span>
+                </div>
+                <div className="arsent__translate">
+                  Пришёл ко мне дантист <span className="contrast"> спрашивая совета </span>
+                </div>
+              </div>
+
+              <h3 className="title ">Отрицание, запрет или вопрос</h3>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span className="contrast_3"> ما </span>
+                  <span> جاء اليوم </span>
+                  <span className="contrast_2"> أحدٌ </span>
+                  <span className="contrast"> مُتأَخِراً </span>
+                </div>
+                <div className="arsent__translate">
+                  Никто сегодня не пришёл <span className="contrast"> опоздавшим </span>
+                </div>
+              </div>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span className="contrast_3"> لا </span>
+                  <span> يَدْخُلْ </span>
+                  <span className="contrast_2"> أحدٌ </span>
+                  <span> قاعةَ الاِمتِحانِ </span>
+                  <span className="contrast"> حامِلاً </span>
+                  <span> حَقيبتَهُ </span>
+                </div>
+                <div className="arsent__translate">
+                  Пусть не заходит ни кто в кабинет экзамена <span className="contrast"> несущим </span> портфель свой
+                </div>
+              </div>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span className="contrast_3"> أ </span>
+                  <span> جَاء اليَومَ </span>
+                  <span className="contrast_2"> أحدٌ </span>
+                  <span className="contrast"> مُتَأَخِراً </span>
+                  <span> ؟ </span>
+                </div>
+                <div className="arsent__translate">
+                  А кто нибудь пришёл <span className="contrast"> опоздавшим </span> ?
+                </div>
+              </div>
+
+              <h3 className="title ">
+                Если <span className="h3ar">الحال</span> является предложением
+              </h3>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> أوْ كَالَّذي مَرَّ على </span>
+                  <span className="contrast_2"> قَرْيَةٍ </span>
+                  <span className="contrast">و هِيَ خَاوِيَةٌ عَلَى عُرُوشِهَا</span>
+                </div>
+                <div className="arsent__translate">
+                  Или над тем кто проходил мимо селения
+                  <span className="contrast"> разрушенного до основания </span>
+                  [Бакара 259]
+                </div>
+              </div>
+
+              <h3 className="title "> В неопределённом состоянии может быть без причины</h3>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> صلّى </span>
+                  <span className="contrast_2"> رسول الله ﷺ </span>
+                  <span className="contrast"> قاعِداً </span>,<span> وصَلَّى ورَاءَهُ </span>
+                  <span className="contrast_2"> رِجالٌ </span>
+                  <span className="contrast"> قِياماً </span>
+                </div>
+                <div className="arsent__translate">
+                  Молился Посланник Аллахъа <span className="contrast"> сидя </span>, за ним мужчины молились
+                  <span className="contrast"> стоя </span>
+                </div>
               </div>
 
               <h2 className="h2ar title">جملة الحال </h2>
 
               <div className="desc">
-                <span className="desc__ar">جملة الحال</span> соединяется с помощью
-                <span className="desc__ar contrast_2"> واو الحال </span>. <br />
-                Но если в предложении глагол прошедшего времени в утвердительной форме то нужно ставить с
-                <span className="desc__ar"> واو الحال </span> частицу
+                <span className="desc__ar">الحال</span> может быть выражен предложением.
+              </div>
+              <div className="desc">
+                Предложение обязательно должно быть связанно с <span className="desc__ar">الحال</span> местоимением или
+                <span className="desc__ar"> واو الحال </span> или местоимением и <span className="desc__ar"> واو الحال </span>
+              </div>
+              <div className="desc">
+                Если в предложении глагол прошедшего времени в утвердительной форме то вместе с<span className="desc__ar"> واو الحال </span>
+                нужно поставить частицу
                 <span className="desc__ar contrast_2"> قد </span>.
               </div>
 
-              <AnimateCard>
-                <div className="arsent arsent_rule">
-                  <div className="arsent__original">
-                    <span className="contrast"> واو الحال </span>
-                    <span> + </span>
-                    <span> الجملة الاسمية </span>
-                    <br />
-                    <span className="contrast"> واو الحال </span>
-                    <span> + </span>
-                    <span className="contrast_2"> قَدْ </span>
-                    <span> + </span>
-                    <span> الجملة الفعلية فعلها ماضٍ مُثْبَتٌ </span>
-                  </div>
-                  <div className="arsent__translate"> </div>
+              <div className="arsent">
+                <div className="arsent__original">
+                  أكل أخي تفاحتي
+                  <span className="contrast">
+                    <span className="contrast_2"> و </span> أنا نائِم
+                  </span>
                 </div>
-              </AnimateCard>
+                <div className="arsent__translate">
+                  Мой брат ел мои яблоки
+                  <span className="contrast"> пока я спал </span>
+                </div>
+              </div>
+
+              <div className="arsent">
+                <div className="arsent__original">
+                  <Word root="جلس" color2 suffix="تُ" />
+                  <span className="contrast"> أسْتَمِعُ إلى تَلاوة القرآن الكريم من الإذاعةِ </span>
+                </div>
+                <div className="arsent__translate">
+                  Я сидел <span className="contrast"> слушая Коран из радио </span>
+                </div>
+              </div>
+
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> خَرَجْنا </span>
+                  <span className="contrast">
+                    <Word root="تَنَزَّهُ" color2 prefix="نَ" />
+                  </span>
+                </div>
+                <div className="arsent__translate">
+                  Вышли <span className="contrast"> прогуляться </span> (гл. 5 породы تَنَزَّهَ)
+                </div>
+              </div>
+
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> جاء الجَريح </span>
+                  <span className="contrast"> دَمُه يَتَدَفَّقُ </span>
+                </div>
+                <div className="arsent__translate">
+                  Пришёл раненный <span className="contrast"> истекая кровью </span>
+                </div>
+              </div>
 
               <div className="arsent">
                 <div className="arsent__original">
@@ -185,19 +328,6 @@ class JumlatuHaalPage extends React.Component {
 
               <div className="arsent">
                 <div className="arsent__original">
-                  قرأ الكتاب
-                  <span className="contrast">
-                    <span className="contrast_2"> و </span> أنا جالِس
-                  </span>
-                </div>
-                <div className="arsent__translate">
-                  Читал книгу
-                  <span className="contrast"> сидя </span>(в состоянии сидения)
-                </div>
-              </div>
-
-              <div className="arsent">
-                <div className="arsent__original">
                   شربت الشاي و القهوة
                   <span className="contrast">
                     <span className="contrast_2"> و </span> أنا في العَمل
@@ -205,24 +335,35 @@ class JumlatuHaalPage extends React.Component {
                 </div>
                 <div className="arsent__translate">
                   Пил чай и кофе
-                  <span className="contrast"> когда был на работе</span> (в состоянии нахождения на работе)
+                  <span className="contrast"> когда был на работе</span>
                 </div>
               </div>
 
               <div className="arsent">
                 <div className="arsent__original">
-                  أكل أخي تفاحتي
-                  <span className="contrast">
-                    <span className="contrast_2"> و </span> أنا نائِم
-                  </span>
+                  <span> وصلتُ إلى مكة </span>
+                  <span className="contrast_2"> و </span>
+                  <span className="contrast"> الشمسُ تَغْرُبُ </span>
                 </div>
                 <div className="arsent__translate">
-                  Мой брат ел мои яблоки
-                  <span className="contrast"> пока я спал </span> (был в состоянии сна)
+                  Прибыл в Мекку
+                  <span className="contrast"> когда солнце заходило </span>
                 </div>
               </div>
 
-              <h2 className=" title"> Дополнительные примеры </h2>
+              <div className="arsent">
+                <div className="arsent__original">
+                  <span> خَرجُوا مِنْ دِيَارِهِمْ </span>
+                  <span className="contrast_2"> وَهُمْ </span>
+                  <span className="contrast"> أُلوفٌ </span>
+                </div>
+                <div className="arsent__translate">
+                  Они покинули свои жилища
+                  <span className="contrast"> (их было) тысячами </span>
+                </div>
+              </div>
+
+              <h2 className=" title"> Отличие от определения (прилагательного) </h2>
               <div className="arsent">
                 <div className="arsent__original">
                   <span> شرب اِبني </span>
@@ -238,38 +379,6 @@ class JumlatuHaalPage extends React.Component {
                 <div className="arsent__original">شرب اِبني الماء الصافِي</div>
                 <div className="arsent__translate">
                   Мой сын пил чистую воду [ Если хотим сказать что вода и была чистой, то используем простое прилагательное ]
-                </div>
-              </div>
-              <div className="arsent">
-                <div className="arsent__original">
-                  <span> جَاءَني </span>
-                  <span className="contrast"> سائلاً </span>
-                  <span className="contrast_2"> طالبٌ </span>
-                </div>
-                <div className="arsent__translate">
-                  Пришёл ко мне <span className="contrast"> спрашивающий </span> студент [ صاحِبُ الحال пришёл перед الحال ]
-                </div>
-              </div>
-              <div className="arsent">
-                <div className="arsent__original">
-                  <span> جَاءَني </span>
-                  <span className="contrast_2"> طالبٌ مُواظِبٌ </span>
-                  <span className="contrast"> مُسْتَأذِناً </span>
-                  <span> في الخُرُوج </span>
-                </div>
-                <div className="arsent__translate">
-                  Пришёл ко мне <span className="contrast"> просящийся </span> войти, старательный студент [ صاحِبُ الحال пришёл с
-                  определением ]
-                </div>
-              </div>
-              <div className="arsent">
-                <div className="arsent__original">
-                  <span> جَاءَني </span>
-                  <span className="contrast_2"> طَبيبُ الأسْنَانٍ </span>
-                  <span className="contrast"> مُسْتَفْتِياً </span>
-                </div>
-                <div className="arsent__translate">
-                  Пришёл ко мне дантист <span className="contrast"> спрашивая совета </span> [ صاحِبُ الحال пришёл в идафе ]
                 </div>
               </div>
 
